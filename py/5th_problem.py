38c51802fa93b81a3cc2c7ed9f91b33d23491fbc
Meek_0 <74826217+meeker0@users.noreply.github.com>
2022-01-10 15:47:45 +0600

    Update 5th_problem.py

--- py/5th_problem.py
+++ py/5th_problem.py
@@ -1,25 +1,39 @@
 # problem form : https://codeforces.com/problemset/problem/214/A
 
-# from sympy import *
+from sympy import *
 
-# x,y = symbols("x,y", intiger = True)
+#  the other values were <sympy.core.add.Add> type. So, I just defined a function to filterOUT these specific types from my list.
+def rem(_list):
+    return list(filter(lambda v: type(v) != Add, _list))
 
-# m,n = input().split(' ')
-# print(f'{int(m) , int(n)}')
-# # solX = roots((x+(int(n)-x**2)**2 - int(m)), x)
-# # solY = roots((int(m) - y**2)**2 + y - int(n), y)
-# sol = solve([x**2 + y - int(n), y**2 + x - int(m)], [x, y])
-# # print(f"{solX}  \n\n\n\n {solY}")
-# print(sol)
+def noLoop(input):
+    x,y = symbols("x,y", intiger = True, negative =False)
+    output = []
+    m,n = input.split(' ')
 
-# UnCool Version
-count = 0
-i = 0
-m,n = input().split(' ')
-big = m if int(m) > int(n) else n
-while(i <= int(big)):
-    for j in range(int(big)+1):
-        if (((i**2)+ j) == int(n)) and ((i+j**2) == int(m)):
-            count = count+1
-    i = i+1
-print(count)
+    # I need to solve these 2 equations seperately. Otherwise, my defined function will not work without loop.
+    solX = rem(solve((x+(int(n)-x**2)**2 - int(m)), x))
+    solY = rem(solve((int(m) - y**2)**2 + y - int(n), y))
+
+    if len(solX) == 0 or len(solY) == 0:
+        print(0)
+    else:
+        output.extend(solX)      # using "Extend" to add multiple values in the list.
+        output.extend(solY)
+        print(int((len(output))/2))  # Obviously, result will come in pairs. So, I need to divide the length of the list by 2.
+
+def Loop(input):
+    count = 0
+    i = 0
+    m,n = input.split(' ')
+    big = m if int(m) > int(n) else n
+    while(i <= int(big)):
+        for j in range(int(big)+1):
+            if (((i**2)+ j) == int(n)) and ((i+j**2) == int(m)):
+                count = count+1
+        i = i+1
+    print(count)
+
+if __name__ == '__main__':
+    noLoop(input())
+    # Loop(input())
