57725383acac589b0926dc19fc96dbea09f35b55
Meek_0 <74826217+meeker0@users.noreply.github.com>
2022-01-11 23:32:02 +0600

    Update 1st_from_C_categoy.py

--- py/1st_from_C_categoy.py
+++ py/1st_from_C_categoy.py
@@ -3,5 +3,5 @@
 
 for s in[*open(0)][2::2]:       # This will make the console input buffer as a list and seperated when \n is found
                                 # [2::2] means [start_pos : end_pos : jump_value]
-    while'()'in s:s=s.replace('()','')
-    print(len(s)//2)
+    while'()'in s:s=s.replace('()','')    # This will remove the nested brackets inside
+    print(len(s)//2)    # Because the unpared brakets remain will be same in number.
